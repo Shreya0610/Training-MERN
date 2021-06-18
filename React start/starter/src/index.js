ec93bbf3c6837b71c8484ca950b3a7016ddd0710
Shreya0610 <sshreyasharma53@gmail.com>
2021-06-18 13:34:32 +0530

    React projects

---
+++ React start/starter/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+  
+    <h1>Welcome to first page</h1>
+ ,
+  document.getElementById('root')
+);
---
+++ Weather/weather/src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonShow from '../src/components/SeasonFind.js';
+
+class App extends React.Component{
+  state = { lat: null, long: null, errorMessage: "" };
+  
+  componentDidMount(){
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({lat: position.coords.latitude, long: position.coords.longitude});
+      },
+      err => {
+        this.setState({errorMessage: err.message});
+      }
+    );
+  }
+
+  render(){
+    if(this.state.errorMessage && !this.state.lat && !this.state.long){
+      return <div><h3>Error: {this.state.errorMessage} </h3></div>
+    }
+    if(!this.state.errorMessage && this.state.lat && this.state.long){
+      return <SeasonShow lat={this.state.lat} long={this.state.long}  />
+    }
+    return <h3>Loading</h3>
+  }
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
